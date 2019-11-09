65e1cfb5d83495d09fd6466292f5afb886d9b17b
Taishawn King <taishawnking1@yahoo.com>
2019-11-09 17:31:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word(sentence1)
 
-  new_array = []
-  new_sentence = sentence1.split(" ")
-  new_sentence.each do |word|
-  new_array << word.reverse
+ #new_sentence = sentence1.split(" ")
+ # new_sentence.each do |word|
+  #new_array << word.reverse
   end # this end matches with the .each
     
      sentence_pluse = new_array.join(" ")
