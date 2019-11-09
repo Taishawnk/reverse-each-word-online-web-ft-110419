6fc9ee8576601b122a3db59faf12df021e782ba2
Taishawn King <taishawnking1@yahoo.com>
2019-11-09 17:07:15 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def reverse_each_word(sentence1)
+
+  new_array = []
+  new_sentence = sentence1.split(" ")
+  new_sentence.each do |word|
+  new_array << word.reverse
+  end # this end matches with the .each
+    
+     sentence_pluse = new_array.join(" ")
+  return sentence_pluse
+  
+new_sentence2 =  sentence2_pluse.split(" ")
+new_array2 = []
+ sentence2_pluse.collect do |word|
+   new_array2 << word.reverse
+    end
+   
+    sentence2_pluse = array2.join(" ")
+ return sentence2_pluse
+ end
